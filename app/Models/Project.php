0e6e8be5816f3265e0e1cc78c6e39960901f7747
nicolozibra1 <nico.zibra@gmail.com>
2023-06-08 23:07:49 +0200

    update model User/Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'image', 'description', 'functionality', 'technologies', 'resource'];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
