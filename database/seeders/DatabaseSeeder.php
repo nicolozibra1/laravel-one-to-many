13618bd8434b7670e8404507e08a805e1dcc9796
nicolozibra1 <nico.zibra@gmail.com>
2023-06-08 19:32:12 +0200

    created Type (model, migration, seeder)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PostSeeder::class,
             TagSeeder::class,
-            ProjectSeeder::class
+            ProjectSeeder::class,
+            TypeSeeder::class
         ]);
     }
 }
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = [
+            'Dynamic',
+            'Responsive',
+            'CMS',
+            'E-commerce',
+            'Web App',
+            'Landing page',
+            'API integration',
+            'Restyling'
+        ];
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($type, '-');
+            $newType->save();
+        }
+    }
+}
